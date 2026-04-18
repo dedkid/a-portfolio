3cc25af91a40b754a31ffd58589091d461f0fdd0
Aki <88832455+dedkid@users.noreply.github.com>
2026-04-19 00:31:06 +0800

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css'; // <--- THIS IS THE KEY TO THE DESIGN
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App />);
